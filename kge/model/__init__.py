0a6632234fcdde1c0c7a049e0b16f25f66b568da
nzteb <patrickmbetz@gmail.com>
2020-08-03 13:16:29 +0200

    Clean up

--- kge/model/__init__.py
+++ kge/model/__init__.py
@@ -20,4 +20,3 @@ from kge.model.simple import SimplE
 # meta models
 from kge.model.reciprocal_relations_model import ReciprocalRelationsModel
 from kge.model.probabilistic_model import ProbabilisticModel
-from kge.model.generative_model import GenerativeModel
